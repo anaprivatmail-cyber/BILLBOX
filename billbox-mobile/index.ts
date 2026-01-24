699fcc9e11f0d95097872bdd31812b7487e0f677
Ana <anaprivatmail@gmail.com>
2026-01-24 16:49:05 +0100

    Add Sentry crash reporting

--- billbox-mobile/index.ts
+++ billbox-mobile/index.ts
@@ -1,8 +1,24 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from 'expo'
+import Constants from 'expo-constants'
+import * as Sentry from 'sentry-expo'
 
-import App from './App';
+import App from './App'
+
+const sentryDsn =
+	process.env.EXPO_PUBLIC_SENTRY_DSN ||
+	(Constants as any)?.expoConfig?.extra?.sentryDsn ||
+	(Constants as any)?.manifest2?.extra?.sentryDsn ||
+	''
+
+if (sentryDsn) {
+	Sentry.init({
+		dsn: sentryDsn,
+		enableInExpoDevelopment: true,
+		debug: false,
+	})
+}
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Sentry.wrap(App))
